7d6fc5f7f605f7bdc6484825f34cd2f6a1577bdc
Ibrahim Nour <ibrahamnour22@gmail.com>
2021-01-22 01:37:24 +0000

    Automatically backed up by Learn

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -8,7 +8,7 @@ const MoviesPage = ({ match, movies }) => (
   <div>
     <MoviesList movies={movies} />
   </div>
-  <MoviesList movies={movies}/}
+  <MoviesList movies={movies}/>
 )
 
 export default MoviesPage
